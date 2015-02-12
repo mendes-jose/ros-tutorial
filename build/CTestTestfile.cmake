3fdabc5368687b24174a810768d5eeaa53c51883
Jose Magno MENDES <jmagno.mf@gmail.com>
2015-02-12 16:54:46 +0100

    Create a catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mendes/Dev/ros-tutorial/src
+# Build directory: /home/mendes/Dev/ros-tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/mendes/Dev/ros-tutorial/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/mendes/Dev/ros-tutorial/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mendes/Dev/ros-tutorial/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mendes/Dev/ros-tutorial/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/mendes/Dev/ros-tutorial/devel/lib:/home/mendes/Dev/ros-tutorial/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/mendes/Dev/ros-tutorial/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/mendes/Dev/ros-tutorial/devel/lib/pkgconfig:/home/mendes/Dev/ros-tutorial/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/mendes/Dev/ros-tutorial/build"
+export PYTHONPATH="/home/mendes/Dev/ros-tutorial/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mendes/Dev/ros-tutorial/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mendes/Dev/ros-tutorial/src:$ROS_PACKAGE_PATH"
